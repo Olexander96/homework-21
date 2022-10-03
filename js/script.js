7cda4be25616d4d24b925d61162fac949f521d80
Olexander96 <o.sheiko96@gmail.com>
2022-10-03 10:46:55 +0300

    for review

---
+++ js/script.js
@@ -0,0 +1,141 @@
+$( document ).ready(function() {
+    
+    // Створення анімованого круга рейтингу
+    const CIRCLE = document.querySelector('.progress-ring--circle')
+    const RADIUS = CIRCLE.r.baseVal.value;
+    const CIRCLE_LENGTH = 2 * Math.PI * RADIUS;
+
+    CIRCLE.style.strokeDasharray = `${CIRCLE_LENGTH} ${CIRCLE_LENGTH}`;
+    CIRCLE.style.strokeDashoffset = CIRCLE_LENGTH;
+
+    function setProgress(rating) {
+        const OFFSET = CIRCLE_LENGTH - rating / 100 * CIRCLE_LENGTH;
+        CIRCLE.style.strokeDashoffset = OFFSET;
+    }
+
+    const RATING = document.querySelector('.rating_value')
+        RATING_VALUE = RATING.textContent * 10;
+    setProgress(RATING_VALUE)//бере значення з рейтингу
+
+    // hover ефект для посилань
+    const LINK_ICON = $('.link_icon');
+
+    LINK_ICON.mouseover( function () {
+        $(this).addClass('link_icon-active');
+    })
+    LINK_ICON.mouseout( function () {
+        $(this).removeClass('link_icon-active')
+    })
+
+    const LINK_TEXT = $('.link_text');
+
+    LINK_TEXT.mouseover( function () {
+        $(this).addClass('link_text-active')
+    })
+    LINK_TEXT.mouseout( function () {
+        $(this).removeClass('link_text-active')
+    })
+
+    // анімація star rating
+    const STAR_1 = $('.star_icon1');
+    const STAR_2 = $('.star_icon2');
+    const STAR_3 = $('.star_icon3');
+    const STAR_4 = $('.star_icon4');
+    const STAR_5 = $('.star_icon5');
+    
+    STAR_1.on('click', function() {
+        STAR_1.addClass('star_icon-activebg');
+    }) 
+
+    STAR_2.on('click', function() {
+        STAR_1.addClass('star_icon-activebg');
+        STAR_2.addClass('star_icon-activebg');
+    }) 
+    STAR_3.on('click', function() {
+        STAR_1.addClass('star_icon-activebg');
+        STAR_2.addClass('star_icon-activebg');
+        STAR_3.addClass('star_icon-activebg');
+    }) 
+    STAR_4.on('click', function() {
+        STAR_1.addClass('star_icon-activebg');
+        STAR_2.addClass('star_icon-activebg');
+        STAR_3.addClass('star_icon-activebg');
+        STAR_4.addClass('star_icon-activebg');
+    }) 
+    STAR_5.on('click', function() {
+        STAR_1.addClass('star_icon-activebg');
+        STAR_2.addClass('star_icon-activebg');
+        STAR_3.addClass('star_icon-activebg');
+        STAR_4.addClass('star_icon-activebg');
+        STAR_5.addClass('star_icon-activebg');
+    }) 
+
+
+    STAR_1.mouseover(function() {
+        STAR_1.addClass('star_icon-active')
+    }) 
+    STAR_1.mouseout(function() {
+        STAR_1.removeClass('star_icon-active')
+    })
+
+    STAR_2.mouseover(function() {
+        STAR_1.addClass('star_icon-active');
+        STAR_2.addClass('star_icon-active')
+    }) 
+    STAR_2.mouseout(function() {
+        STAR_1.removeClass('star_icon-active');
+        STAR_2.removeClass('star_icon-active');
+    })
+
+    STAR_3.mouseover(function() {
+        STAR_1.addClass('star_icon-active');
+        STAR_2.addClass('star_icon-active');
+        STAR_3.addClass('star_icon-active');
+    }) 
+    STAR_3.mouseout(function() {
+        STAR_1.removeClass('star_icon-active');
+        STAR_2.removeClass('star_icon-active');
+        STAR_3.removeClass('star_icon-active');
+    })
+
+    STAR_4.mouseover(function() {
+        STAR_1.addClass('star_icon-active');
+        STAR_2.addClass('star_icon-active');
+        STAR_3.addClass('star_icon-active');
+        STAR_4.addClass('star_icon-active');
+    }) 
+    STAR_4.mouseout(function() {
+        STAR_1.removeClass('star_icon-active');
+        STAR_2.removeClass('star_icon-active');
+        STAR_3.removeClass('star_icon-active');
+        STAR_4.removeClass('star_icon-active');
+    })
+
+    STAR_5.mouseover(function() {
+        STAR_1.addClass('star_icon-active');
+        STAR_2.addClass('star_icon-active');
+        STAR_3.addClass('star_icon-active');
+        STAR_4.addClass('star_icon-active');
+        STAR_5.addClass('star_icon-active');
+    }) 
+    STAR_5.mouseout(function() {
+        STAR_1.removeClass('star_icon-active');
+        STAR_2.removeClass('star_icon-active');
+        STAR_3.removeClass('star_icon-active');
+        STAR_4.removeClass('star_icon-active');
+        STAR_5.removeClass('star_icon-active');
+    })
+
+    
+    
+});
+
+
+
+
+
+    
+
+
+
+    
